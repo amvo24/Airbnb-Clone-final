2acaa2ed57b22ef1615ed03dcde02d13070f202c
Andrew <andrewvo1024@gmail.com>
2022-07-06 10:29:28 -0500

    validate spots added

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,10 +3,39 @@ const {requireAuth } = require('../../utils/auth');
 const {Spot, Image, User} = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-
-
 const router = express.Router();
 
+const validateSpots = [
+  check('address')
+    .exists({ checkFalsy: true })
+    .withMessage('Street address is required'),
+  check('city')
+    .exists({ checkFalsy: true})
+    .withMessage('City is required'),
+  check('state')
+    .exists({checkFalsy: true})
+    .withMessage('State is required'),
+  check('name')
+    .exists({ checkFalsy: true })
+    .isLength({ max: 50 })
+    .withMessage('Name must be less than 50 characters'),
+  check('country')
+    .exists({checkFalsy: true})
+    .withMessage('Country is required'),
+  check('lat')
+    .exists({checkFalsy: true})
+    .withMessage('Latitude is not valid'),
+  check('lng')
+    .exists({checkFalsy: true})
+    .withMessage('Longitude is not valid'),
+  check('description')
+    .exists({checkFalsy: true})
+    .withMessage('Description is required'),
+  check('price')
+    .exists({checkFalsy: true})
+    .withMessage('Price per day is required'),
+  handleValidationErrors
+];
 
 // GET all spots
 router.get('/', async (req,res) => {
