801e3e3023f4249178361330a8bbf115ba51d080
Andrew <andrewvo1024@gmail.com>
2022-07-10 17:50:16 -0500

    debug

--- backend/db/models/images.js
+++ backend/db/models/images.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull:false,
       autoIncrement: true,
       primaryKey: true,
-      type: DataTypes.STRING
+      type: DataTypes.INTEGER
     },
     url: {
       type: DataTypes.STRING
