98a08942985d9aa38c060b5964bea329f70f9cc8
Andrew <andrewvo1024@gmail.com>
2022-07-10 18:19:28 -0500

    debug

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // define association here k
       Review.belongsTo(models.User, {foreignKey: 'userId'});
       Review.belongsTo(models.Spot, {foreignKey: 'spotId'});
       Review.hasMany(models.Image, {foreignKey: 'reviewId', as: 'images', onDelete: 'CASCADE', hooks: true});
