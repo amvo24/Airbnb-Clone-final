22e6369f59c87f96c12443af16fee7ab794913d0
Andrew <andrewvo1024@gmail.com>
2022-07-28 20:15:56 -0500

    review by spot id is finally done

--- backend/db/seeders/20220706221145-reviews.js
+++ backend/db/seeders/20220706221145-reviews.js
@@ -5,17 +5,38 @@ module.exports = {
     return queryInterface.bulkInsert('Reviews', [
       {
 
-        review: 'I wanna go home',
+        review: 'I am user 1 and I did not like this place. It scared me.',
         stars: 2,
         spotId: 2,
         userId: 1
       },
       {
 
-        review: 'I love this place',
+        review: 'I am user 1 and I did love this place.',
+        stars: 5,
+        spotId: 1,
+        userId: 1
+      },
+      {
+
+        review: 'I am user 1 and this place was ok.',
+        stars: 3,
+        spotId: 3,
+        userId: 1
+      },
+      {
+
+        review: 'I am user id 2 and I loved this place. Will be coming back soon!',
         stars: 5,
         spotId: 1,
         userId: 2
+      },
+      {
+
+        review: 'I am user id 3 and I wanna move to this place, I loved it a lot!',
+        stars: 5,
+        spotId: 3,
+        userId: 3
       },
     ])
   },
