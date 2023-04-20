97930ac69551d744e190a5094aec1a3f53ca6868
Andrew <andrewvo1024@gmail.com>
2023-04-20 10:25:24 -0500

    adjustments made to spots css

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -19,27 +19,30 @@ const Spots = () => {
   }, [dispatch, JSON.stringify(spots)]);
 
   return (
-    <div className="all-spots-div">
-      {spots.map((spot, index) => (
-        <div className="spot-div-2345" key={index}>
-          <Link to={`/spots/${spot.id}`} className="spot-link" key={spot.id}>
-            <div className="spot-div">
-              <div className="img-div">
-                <img className="spot-img"src={spot.previewImage} alt="preview of spot"></img>
-              </div>
-              <div className="spot-info">
-                <div className="spot-info-first-line">
-                  <div className="spot-info-name">{spot.name}</div>
-                <div className="Spot-info-AvgStar">{spot?.avgStarRating}</div>
+    <div className="middle_container">
+      <div className="all-spots-div">
+        {spots.map((spot, index) => (
+          <div className="spot-div-2345" key={index}>
+            <Link to={`/spots/${spot.id}`} className="spot-link" key={spot.id}>
+              <div className="spot-div">
+                <div className="img-div">
+                  <img className="spot-img"src={spot.previewImage} alt="preview of spot"></img>
                 </div>
-                <div className="spot-city-state">{`${spot.city}, ${spot.state}`}</div>
-                <div className="spot-beds">{`${spot.beds} beds`}</div>
-                <div className="spot-price">{`$${spot.price} night`}</div>
+                <div className="spot-info">
+                  <div className="spot-info-first-line">
+                    <div className="spot-info-name">{spot.name}</div>
+                  <div className="Spot-info-AvgStar">{spot?.avgStarRating}</div>
+                  </div>
+                  <div className="spot-city-state">{`${spot.city}, ${spot.state}`}</div>
+                  <div className="spot-beds">{`${spot.beds} beds`}</div>
+                  <div className="spot-price">{`$${spot.price} night`}</div>
+                </div>
               </div>
-            </div>
-          </Link>
-        </div>
-      ))}
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div>FOOTER</div>
     </div>
   );
 
